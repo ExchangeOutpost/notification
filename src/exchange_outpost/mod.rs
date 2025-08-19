ea8dc46b1b0913dbc5dedaffe626dbbfbe3ea6c2
AlessandroRuggiero <50781152+AlessandroRuggiero@users.noreply.github.com>
2025-08-19 10:35:46 +0200

    webhook implemented

--- src/exchange_outpost/mod.rs
+++ src/exchange_outpost/mod.rs
@@ -4,4 +4,4 @@ mod notifications;
 
 pub use candle::Candle;  
 pub use fin_data::FinData;
-pub use notifications::send_notification;
+pub use notifications::schedule_webhook;
--- src/exchange_outpost/notifications.rs
+++ src/exchange_outpost/notifications.rs
@@ -1,16 +1,17 @@
-use extism_pdk::host_fn;
+use extism_pdk::{host_fn, WithReturnCode};
 
 
 #[host_fn]
 extern "ExtismHost" {
-    fn add_notification(notification: String); 
+    fn add_notification(notification_type: String, notification_target: String, body: String); 
 }
 
-pub fn send_notification(notification: String) {
-    unsafe{
-        let res = add_notification(notification);
+pub fn schedule_webhook(path:&str, body: &str) -> Result<(), WithReturnCode<extism_pdk::Error>> {
+    unsafe {
+        let res = add_notification("webhook".into(), path.into(), body.into());
         if res.is_err() {
-            eprintln!("Failed to send notification");
+            return Err(WithReturnCode::new(extism_pdk::Error::new(std::io::Error::new(std::io::ErrorKind::Other, "impossible to send notification")), 6));
         }
+        Ok(())
     }
 }
